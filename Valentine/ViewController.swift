cc036e8ea14daa8ece818c5389a9b1b08091d9c0
Natsuko Nishikata <natsu@natsuapps.com>
2015-02-13 22:27:56 +0900

    Size Class sample

---
+++ Valentine/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Valentine
+//
+//  Created by Natsuko Nishikata on 2015/02/10.
+//  Copyright (c) 2015年 Natsuko Nishikata. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
---
+++ ValentineTests/ValentineTests.swift
@@ -0,0 +1,36 @@
+//
+//  ValentineTests.swift
+//  ValentineTests
+//
+//  Created by Natsuko Nishikata on 2015/02/10.
+//  Copyright (c) 2015年 Natsuko Nishikata. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class ValentineTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
